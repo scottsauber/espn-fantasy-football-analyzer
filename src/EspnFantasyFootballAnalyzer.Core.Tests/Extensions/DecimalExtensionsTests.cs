d47d40cb0be1bd434bd8f005585ef288d4a82e66
Scott Sauber <stsauber@gmail.com>
2021-10-03 00:31:45 -0500

    add decimal extensions

---
+++ src/EspnFantasyFootballAnalyzer.Core.Tests/Extensions/DecimalExtensionsTests.cs
@@ -0,0 +1,19 @@
+﻿using EspnFantasyFootballAnalyzer.Core.Extensions;
+using FluentAssertions;
+using Xunit;
+
+namespace EspnFantasyFootballAnalyzer.Core.Tests.Extensions
+{
+    public class DecimalExtensionsTests
+    {
+        [Fact]
+        public void ShouldTruncateAfterTwoDecimalPlaces()
+        {
+            var value = 100.240000007m;
+
+            var result = value.TruncateAfterTwoDecimalPlaces();
+
+            result.Should().Be(100.24m);
+        }
+    }
+}
---
+++ src/EspnFantasyFootballAnalyzer.Core/Extensions/DecimalExtensions.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace EspnFantasyFootballAnalyzer.Core.Extensions
+{
+    public static class DecimalExtensions
+    {
+        public static decimal TruncateAfterTwoDecimalPlaces(this decimal value) => Math.Truncate(100 * value) / 100;
+    }
+}
