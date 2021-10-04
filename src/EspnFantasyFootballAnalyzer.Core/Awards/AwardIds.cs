491b7f650656556ce3b700eb8ec22fc7f929361b
Scott Sauber <stsauber@gmail.com>
2021-10-03 22:46:26 -0500

    add biggest blowout award to AwardService

--- src/EspnFantasyFootballAnalyzer.Core/Awards/AwardIds.cs
+++ src/EspnFantasyFootballAnalyzer.Core/Awards/AwardIds.cs
@@ -7,5 +7,6 @@ namespace EspnFantasyFootballAnalyzer.Core.Awards
         public static Guid MostPointsAward => new("4A162503-E9F4-4EDD-B1F1-775688F4913C");
         public static Guid LeastPointsAward => new("5A5C9D6B-B16F-42C2-9F88-1DDFA5F3D9E4");
         public static Guid BiggestBlowoutAward => new("D0C16349-6213-4A06-8701-C784773D94B7");
+        public static Guid SmallestMarginOfVictoryAward => new("6AA49C05-E16A-4B31-AC38-71B2E965F96D");
     }
 }
