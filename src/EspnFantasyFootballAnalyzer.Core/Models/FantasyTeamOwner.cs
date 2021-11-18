1373dbf99591e56b844b9b6c580b0ef6c7764522
Scott Sauber <stsauber@gmail.com>
2021-11-17 22:10:01 -0600

    remove unused autogenerated models from ESPN API

--- src/EspnFantasyFootballAnalyzer.Core/Models/FantasyTeamOwner.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace EspnFantasyFootballAnalyzer.Core.Models
-{
-    public record FantasyTeamOwner
-    {
-        public int Id { get; init; }
-        public string FirstName { get; init; }
-        public string LastName { get; init; }
-    }
-}
